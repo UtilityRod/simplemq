fd4331c3474222472fc78ffd62d5a05234ce52a1
Daniel Rodriguez <drodriguez2933@gmail.com>
2023-01-07 23:15:36 -0500

    Set up project directory

---
+++ core/server.go
@@ -0,0 +1,7 @@
+package core
+
+import "fmt"
+
+func RunServer() {
+	fmt.Println("Running server")
+}
---
+++ test_server.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"smq/core"
+)
+
+func main() {
+	fmt.Println("Hello")
+	core.RunServer()
+}
